fe797ca6654f053523169ea64e027fdc6963b31b
polakv93 <>
2025-09-27 10:17:00 +0200

    feat: add CLI flag support for help message

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"go_transform/transform"
 )
 
 func main() {
+	help := flag.Bool("help", false, "Display help")
+	flag.Parse()
+
+	if *help {
+		printHelp()
+		return
+	}
+
+	x := flag.Args()
+	fmt.Print(x)
+
 	err := transform.ExecuteTransform("exampleFiles/transform.json")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func printHelp() {
+	fmt.Println("Usage: [flags] <transform file path>")
+	fmt.Println("Available flags:")
+	flag.PrintDefaults()
 }
